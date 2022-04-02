8dec80678bc8e83430a8f9c76ac64188de8d404b
Jonas Otten <jonadev95@posteo.org>
2022-04-02 15:04:54 +0100

    verbose

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ then
 	curl -o ndk.zip https://dl.google.com/android/repository/$NDK_DIR-linux.zip
 	unzip ndk.zip
 fi
-
+set -x
 export PATH="$NDK_DIR/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH"
 RUSTFLAGS="-L $NDK_DIR/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/aarch64-linux-android/30" cargo +nightly build --target=aarch64-linux-android --release -Zbuild-std
