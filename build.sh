874d914a998b5e620d6934dec172d3cc2c5edffe
Jonas Otten <jonadev95@posteo.org>
2022-04-02 15:31:36 +0100

    cc

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ then
 fi
 set -x
 export PATH="$NDK_DIR/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH"
+export CC=aarch64-linux-android30-clang
 RUSTFLAGS="-L $NDK_DIR/toolchains/llvm/prebuilt/linux-x86_64/lib64/clang/12.0.8/lib/linux/aarch64/ -L deps/ -L $NDK_DIR/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/aarch64-linux-android/30" cargo build --target=aarch64-linux-android --release # -Zbuild-std
 
 
