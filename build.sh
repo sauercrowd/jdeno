d61993f6868aa26ab7d3173dd70e0108037dcbf8
Jonas Otten <jonadev95@posteo.org>
2022-04-02 15:17:05 +0100

    build script

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 NDK_DIR=android-ndk-r23b
-if ! [ -f $NDK_DIR ]
+if ! [ -d $NDK_DIR ]
 then
 	curl -o ndk.zip https://dl.google.com/android/repository/$NDK_DIR-linux.zip
 	unzip ndk.zip
 fi
 set -x
 export PATH="$NDK_DIR/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH"
-RUSTFLAGS="-L $NDK_DIR/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/aarch64-linux-android/30" cargo +nightly build --target=aarch64-linux-android --release -Zbuild-std
+RUSTFLAGS="-L deps/ -L $NDK_DIR/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/aarch64-linux-android/30" cargo build --target=aarch64-linux-android --release # -Zbuild-std
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,5 @@ use deno_core;
 
 pub fn create_runtime(){
     let runtime = deno_core::JsRuntime::new(deno_core::RuntimeOptions::default());
-    println!(runtime.execute_script("", fs::read_to_string("index.js").unwrap()).unwrap());
+    //println!(runtime.execute_script("", fs::read_to_string("index.js").unwrap()).unwrap());
 }
