9cccd474e9cff87499b6499ebbfc9047b2df1455
sauercrowd <sauercrowd@posteo.no>
2022-04-03 01:38:03 +0100

    memory loader

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
-NDK_DIR=android-ndk-r23b
+NDK_DIR="${NDK_DIR:=`pwd`/android-ndk-r23b}"
+
+mkdir -p .cargo
+echo "[target.aarch64-linux-android]" > .cargo/config
+echo "ar = \"$NDK_DIR/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar\"" >> .cargo/config
+echo "linker = \"$NDK_DIR/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android30-clang\"" >> .cargo/config
+
 if ! [ -d $NDK_DIR ]
 then
-	curl -o ndk.zip https://dl.google.com/android/repository/$NDK_DIR-linux.zip
+	curl -o ndk.zip https://dl.google.com/android/repository/`basename $NDK_DIR`-linux.zip
 	unzip ndk.zip
 fi
 set -x
 export PATH="$NDK_DIR/toolchains/llvm/prebuilt/linux-x86_64/bin/:$PATH"
 RUSTFLAGS="-L $NDK_DIR/toolchains/llvm/prebuilt/linux-x86_64/lib64/clang/12.0.8/lib/linux/aarch64/ -L deps/ -L $NDK_DIR/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/aarch64-linux-android/30" cargo build --target=aarch64-linux-android --release # -Zbuild-std
-
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,81 @@
+use std::error::Error;
+use deno_core::futures::FutureExt;
+use deno_core::resolve_import;
+use deno_core::ModuleLoader;
+use deno_core::ModuleSource;
+use std::rc::Rc;
+use tokio::runtime::Builder;
 
-use deno_core;
-
-pub fn create_runtime(){
-    let runtime = deno_core::JsRuntime::new(deno_core::RuntimeOptions::default());
-    //println!(runtime.execute_script("", fs::read_to_string("index.js").unwrap()).unwrap());
+pub struct MemoryLoader {
+    files: std::collections::HashMap<String, String>,
 }
+
+impl ModuleLoader for MemoryLoader {
+    fn resolve(
+        &self,
+        specifier: &str,
+        referrer: &str,
+        _is_main: bool,
+    ) -> std::result::Result<deno_core::url::Url, deno_core::anyhow::Error> {
+        Ok(resolve_import(specifier, referrer)?)
+    }
+    fn load(
+        &self,
+        module_specifier: &deno_core::url::Url,
+        _maybe_referrer: std::option::Option<deno_core::url::Url>,
+        _is_dynamic: bool,
+    ) -> std::pin::Pin<
+        std::boxed::Box<
+            (dyn std::future::Future<
+                Output = std::result::Result<deno_core::ModuleSource, deno_core::anyhow::Error>,
+            > + 'static),
+        >,
+    > {
+        let module_specifier = module_specifier.clone();
+        let path = module_specifier.as_str();
+        let files_clone = self.files.clone();
+        println!("URL: {}", path);
+        let code = files_clone.get(path).expect("No file with name found");
+        let x = code.clone();
+        async move {
+            let module = ModuleSource {
+                code: x,
+                module_type: deno_core::ModuleType::JavaScript,
+                module_url_specified: module_specifier.to_string(),
+                module_url_found: module_specifier.to_string(),
+            };
+            Ok(module)
+        }
+        .boxed_local()
+    }
+}
+
+#[test]
+
+fn run_js() {
+    let mut files = std::collections::HashMap::new();
+    files.insert("file:///index".to_string(), std::fs::read_to_string("index.js").unwrap());
+    files.insert("file:///lockbox.js".to_string(), std::fs::read_to_string("lockbox.js").unwrap());
+    let module_loader = MemoryLoader { files: files };
+    let mut js_runtime = deno_core::JsRuntime::new(deno_core::RuntimeOptions {
+        module_loader: Some(Rc::new(module_loader)),
+        ..deno_core::RuntimeOptions::default()
+    });
+
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let main_module = deno_core::resolve_path("/index").unwrap();
+
+    let future = async move {
+        let mod_id = js_runtime.load_main_module(&main_module, None).await?;
+        let _ = js_runtime.mod_evaluate(mod_id);
+        js_runtime.run_event_loop(false).await?;
+        Ok::<(), deno_core::anyhow::Error>(()) 
+    };
+    runtime.block_on(future).unwrap();
+}
+
+// pub fn create_runtime() {}
